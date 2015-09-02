651ab3298f2f6fba40d30123a8d97665f0ed8289
Sebastian Sandqvist <seb@sparque.me>
2015-09-02 12:52:17 -0700

    es6, istanbul, eslint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // ----- dependencies
 // ---------------------------------------
-var sift = require('sift-distance');
+let sift = require('sift-distance');
 
 // ----- sort function
 //		--	@param arr {Array} strings to sort
@@ -12,13 +12,16 @@ var sift = require('sift-distance');
 // ---------------------------------------
 function sort(arr, str, max) {
 
+	const opts = {
+		maxDistance: max || null,
+		maxOffset: sort.maxOffset
+	};
+
 	if (max) {
 		arr = arr.filter(function(el) {
-			return sift(el, str, {maxDistance: max, maxOffset: sort.maxOffset}) < max;
+			return sift(el, str, opts) < max;
 		});
 	}
-
-	var opts = {maxOffset: sort.maxOffset};
 
 	return arr.sort(function(a, b) {
 		return sift(a, str, opts) - sift(b, str, opts);
